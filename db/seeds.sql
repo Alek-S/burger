63860a2816627a9dc89ea5fdefab54466db3f7b2
Alek Shnayder <alek.shnayder@gmail.com>
2017-05-07 20:14:01 -0500

    Database schema and seed data

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured, Date)
+    VALUES  ('Mushroom Swiss', FALSE, CURRENT_TIMESTAMP );
+
+INSERT INTO burgers (burger_name, devoured, Date)
+    VALUES  ('Angus', FALSE, CURRENT_TIMESTAMP );
+    
+INSERT INTO burgers (burger_name, devoured, Date)
+    VALUES  ('BBQ Bacon', FALSE, CURRENT_TIMESTAMP );
